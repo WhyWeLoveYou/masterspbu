39080c8610869e1f5050b5b0fef4d070cebbb35d
WhyWeLoveYou <alifventura613@gmail.com>
2024-08-22 10:21:42 +0700

    Premium

--- __manifest__.py
+++ __manifest__.py
@@ -24,12 +24,14 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
+        'views/itemmenu.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
         'demo/demo.xml',
     ],
+    'application': True,
+    'installable': True,
 }
