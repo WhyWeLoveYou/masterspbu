d5221e61c1b507cc019a94b71605b79c4eeb8e4c
rensyci <arsylananc@gmail.com>
2024-08-22 09:33:59 +0700

    bebas

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
         Long description of module's purpose
     """,
 
-    'author': "pkl pkl",
+    'author': "pkl aja",
     'website': "https://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
