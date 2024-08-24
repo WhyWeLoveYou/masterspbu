7acc27666feca5dbbec3628e3c9aebeb67fd4b7f
rensyci <arsylananc@gmail.com>
2024-08-24 09:03:22 +0700

    tambah

--- models/models.py
+++ models/models.py
@@ -18,6 +18,15 @@ class Premium2(models.Model):
     tanggal = fields.Date(string='Tanggal')
     premium2_line_ids = fields.One2many('master_spbu.premium2_line', 'premium2_ids', string='Premium2 Line Items')
 
+class Premium3(models.Model):
+    _name = 'master_spbu.premium3'
+    _description = 'master_spbu.premium3'
+
+    name = fields.Char(string='Name')
+    tanggal = fields.Date(string='Tanggal')
+    premium3_line_ids = fields.One2many('master_spbu.premium3_line', 'premium3_ids', string='Premium3 Line Items')
+
+
 class PremiumLine(models.Model):
     _name = 'master_spbu.premium_line'
     _description = 'master_spbu.premium_line'
@@ -70,4 +79,24 @@ class Premium2Line(models.Model):
     tebusan = fields.Float(string='tebusan', default=0.0)
     kiriman = fields.Float(string='kiriman', default=0.0)
     sisa_do = fields.Float(string='sisa do', default=0.0)
+
+class Premium3Line(models.Model):
+    _name = 'master_spbu.premium3_line'
+    _description = 'master_spbu.premium3_line'
+
+    premium3_ids = fields.Many2one('master_spbu.premium3', string='Premium3')
+    tanggal = fields.Char(string='Tanggal')
+    premium = fields.Float(string='Premium', default=0.0)
+    solar = fields.Float(string='Solar', default=0.0)
+    pertamax = fields.Float(string='Pertamax', default=0.0)
+    pertalite = fields.Float(string='Pertalite', default=0.0)
+    dexlite = fields.Float(string='Dexlite', default=0.0)
+
+    total = fields.Float(string='Total Tebusan', compute='_compute_total_tebusan')
+
+    @api.depends('premium', 'solar', 'pertamax', 'pertalite', 'dexlite')
+    def _compute_total_tebusan(self):
+        for record in self:
+            record.total = record.premium + record.solar + record.pertamax + record.pertalite + record.dexlite
+
 
